feb5e8e6f57fff4bfd999c853e22346730e0b7fe
UDFKT <udfkt1@dingtalk.com>
2021-09-06 16:01:27 +0800

    temp
    Signed-off-by: UDFKT <udfkt1@dingtalk.com>

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ declare global {
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`Current game tick is ${Game.time}`);
+  console.log(`Current tick is ${Game.time}`);
   if (Game.time % 1000 === 0) {
     // 刷新缓存能量源
     Memory.sourceMap = {};
