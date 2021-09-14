6d72a27998b7d5acac1b03faeaffa1aa7b252940
UDFKT <udfkt1@dingtalk.com>
2021-09-14 17:08:52 +0800

    temp

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,8 @@ declare global {
 
   // Memory extension samples
   interface Memory {
-    sourceMap: { [sourceID: string]: Source };
+    uuid: number;
+    log: any;
   }
 
   interface CreepMemory {
@@ -34,16 +35,6 @@ declare global {
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`Current tick is ${Game.time}`);
-  if (Game.time % 1000 === 0) {
-    // 刷新缓存能量源
-    Memory.sourceMap = {};
-    for (const roomName in Game.rooms) {
-      Game.rooms[roomName].find(FIND_SOURCES).forEach(source => {
-        Memory.sourceMap[source.id] = source;
-        console.log("重新缓存能量源对象：" + JSON.stringify(source));
-      });
-    }
-  }
   // 删除无效creep缓存
   for (const name in Memory.creeps) {
     if (!(name in Game.creeps)) {
